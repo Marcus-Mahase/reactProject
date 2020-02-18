cd26a4d4420ad847816607cdbf31c2cc38b44b28
Marcus Mahase <marcus.mahase@outlook.com>
2020-02-18 12:30:25 -0400

    Updates

---
+++ precache-manifest.7fe1eae1776f3011a1019adce86b0e2b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49a3f14bbbf085f05f0e15ffcd4a9fd9",
+    "url": "/reactProject/index.html"
+  },
+  {
+    "revision": "ea2b58af1f6321971324",
+    "url": "/reactProject/static/css/main.86edb36f.chunk.css"
+  },
+  {
+    "revision": "7b5c22c0c8dc46d4aa6a",
+    "url": "/reactProject/static/js/2.d26dae60.chunk.js"
+  },
+  {
+    "revision": "8ae279e0396a82bcb324f2e1f01716e9",
+    "url": "/reactProject/static/js/2.d26dae60.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea2b58af1f6321971324",
+    "url": "/reactProject/static/js/main.82eab38c.chunk.js"
+  },
+  {
+    "revision": "2c1889ae4a0c7edab69a",
+    "url": "/reactProject/static/js/runtime-main.0cbeed36.js"
+  }
+]);
